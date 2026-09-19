4ab1abbde47d7075accaae6a514b018f4f302fca
Tarsila Barros Fritz <tarsilabarrosfritz03@gmail.com>
2026-09-19 19:30:51 -0300

    feat: add basic number guessing game

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿public class NumberGuessingGame 
+{
+    public static void Main(string[] args)
+    {
+        bool isCorrect;
+
+        Random random = new Random();
+        var randomNumber = random.NextInt64(0, 101);
+
+        Console.WriteLine($"Number generated: {randomNumber}");
+
+        do
+        {
+            Console.Write("Inform a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());            
+
+            if (userInput != randomNumber)
+            {
+                Console.WriteLine("Wrong!\nTry again...");
+                isCorrect = false;
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+                isCorrect = true;
+            }
+        } while (!isCorrect);        
+    }
+}
