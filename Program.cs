5611dcf668544e24e3723373f7f618a97bcea400
Tarsila Barros Fritz <tarsilabarrosfritz03@gmail.com>
2026-09-19 22:33:17 -0300

    feat: add difficulty levels and attempt limits

--- Program.cs
+++ Program.cs
@@ -3,27 +3,63 @@
     public static void Main(string[] args)
     {
         bool isCorrect;
+        int rangeLimit = 0;
+        int maxAttempts = 0;
+        int attempts = 0;
+
+        Console.WriteLine("\nChoose your difficulty level:");
+        Console.WriteLine("[1] Easy - 0 to 10 | 5 attempts\n[2] Medium - 0 to 50 | 7 attempts\n[3] Hard - 0 to 100 | 10 attempts");
+        Console.Write("\nEnter your choice: ");
+        int gameLevel = Convert.ToInt16(Console.ReadLine());
+
+        switch (gameLevel)
+        {
+            case 1: 
+                rangeLimit = 11;
+                maxAttempts = 5;
+                break;
+            case 2:
+                rangeLimit = 51;
+                maxAttempts = 7;
+                break;
+            case 3:
+                rangeLimit = 101;
+                maxAttempts = 10;
+                break;
+            default:
+                Console.WriteLine("Invalid option.");
+                break;  
+        }
 
         Random random = new Random();
-        var randomNumber = random.NextInt64(0, 101);
+        var randomNumber = random.NextInt64(0, rangeLimit);
 
-        Console.WriteLine($"Number generated: {randomNumber}");
+        Thread.Sleep(3000);
+        Console.WriteLine($"\nThe number has been generated!");
 
         do
         {
-            Console.Write("Inform a number: ");
-            int userInput = Convert.ToInt32(Console.ReadLine());            
+            Console.Write("\nInform a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
 
-            if (userInput != randomNumber)
+            if (attempts == maxAttempts)
             {
-                Console.WriteLine("Wrong!\nTry again...");
+                Console.WriteLine($"\nYou used all attempts available.");
+                Thread.Sleep(2000);
+                Console.WriteLine($"\nThe correct number was: {randomNumber}.");
+                break;
+            } 
+            else if (userInput != randomNumber && attempts < maxAttempts)
+            {
+                Console.WriteLine("\nWrong!\nTry again...");
+                attempts++;
                 isCorrect = false;
             }
             else
             {
-                Console.WriteLine("Correct!");
+                Console.WriteLine("\nCorrect!");
                 isCorrect = true;
             }
-        } while (!isCorrect);        
+        } while (!isCorrect);
     }
 }
--- obj/Debug/net10.0/number-guessing-game.AssemblyInfo.cs
+++ obj/Debug/net10.0/number-guessing-game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("number-guessing-game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ab1abbde47d7075accaae6a514b018f4f302fca")]
 [assembly: System.Reflection.AssemblyProductAttribute("number-guessing-game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("number-guessing-game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
